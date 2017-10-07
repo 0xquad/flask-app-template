ddfdc5912e7343d72a9a4287d9ff6140688c7288
Alexandre Hamelin <alexandre.hamelin@gmail.com>
2017-10-07 01:48:29 +0000

    Fix a runtime warning about SQLAlchemy

--- app.tmpl/models.py
+++ app.tmpl/models.py
@@ -10,6 +10,7 @@ from flask_migrate import Migrate
 from {{PROJECTNAME}} import app
 
 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{}'.format(
     os.path.join(app.root_path, app.name + '.db'))
 
