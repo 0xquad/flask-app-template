85897c2bf4e4e9c89db6111894879d18fef577dd
Alexandre Hamelin <alexandre.hamelin@gmail.com>
2017-10-18 03:33:03 +0000

    Use the login manager and set a default app secret key

--- app.tmpl/__init__.py
+++ app.tmpl/__init__.py
@@ -4,7 +4,12 @@
 
 
 from flask import Flask
+from flask_login import LoginManager
 app = Flask(__name__)
+app.secret_key = 'default-secret-key'
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
 # Import anything that depended on `app`
 from {{PROJECTNAME}}.views import *
