06f6dc85a62b7d85b3487217905e075a61d7c2ee
Alexandre Hamelin <alexandre.hamelin@gmail.com>
2017-10-19 03:06:29 +0000

    Added a default and generic login/logout implementation

--- app.tmpl/views.py
+++ app.tmpl/views.py
@@ -30,3 +30,46 @@ def home():
     return render('home.html')
 
 
+def login_user(user_id, passwd):
+#   profile = UserProfile.query.filter_by(email_addr=user_id).first()
+#   if profile and profile.check_password(password):
+#       login_user(profile)
+#       return True
+#   else:
+#       return False
+    pass
+
+def check_safe_url(url):
+    pass
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    email = None
+    error = None
+    if request.method == 'POST':
+        email = request.form.get('email', None)
+        password = request.form.get('password', None)
+            next_url = request.args.get('next', '')
+            if not next_url.startswith('/'):
+                next_url = None
+            return redirect(next_url or url_for('home'))
+
+        if login_user(email, password):
+            # the 'next' parameter is automatically added to the URL
+            # when the user accesses a route with @login_required while
+            # not authenticated
+            next_url = request.args.get('next', '')
+            #check_safe_url(next_url)
+            if not next_url.startswith('/'):
+                next_url = None
+            return redirect(next_url or url_for('home'))
+        else:
+            error = 'Invalid credentials.'
+
+    return render('login.html', email=email, error=error)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
