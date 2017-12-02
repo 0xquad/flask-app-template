f99fd2aa6eb8ac638d4a7178f9a97fd65fcd64a9
Alexandre Hamelin <alexandre.hamelin@gmail.com>
2017-12-02 17:42:37 +0000

    Allow the reinitialization of the environment

--- init.sh
+++ init.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 #
-# Copyright (c) 2015, Alexandre Hamelin <alexandre.hamelin gmail.com>
+# Copyright (c) 2017, Alexandre Hamelin <alexandre.hamelin gmail.com>
 #
 # Initializes the project virtualenv by installing requirements.
 
 set -e
+
+[[ "$1" = "-h" ]] && {
+    echo "usage: $0 projectname  (first time execution)"
+    echo "       $0  (to reinitialize the virtualenv)"
+    exit
+}
 
 [[ -e requirements.txt ]] || {
     echo 'not in the project directory? missing requirements.txt'
@@ -21,26 +27,39 @@ set -e
     exit 1
 }
 
-appname=$1
-[[ -z "$appname" || "$appname" =~ [^a-zA-Z0-9_] ]] && {
-    echo "invalid project name '$appname' (must be alphanum only)"
-    exit 1
+# placeholder for later
+appname=myapp
+
+[[ -d app.tmpl ]] && {
+    # Check for first time execution to initialize project files.
+    appname=$1
+    [[ -z "$appname" || "$appname" =~ [^a-zA-Z0-9_] ]] && {
+        echo "error: invalid project name '$appname' (must be alphanum only)"
+        exit 1
+    }
+    mv app.tmpl "$appname"
+    # Replace placeholders for the real project name
+    sed -i -e "s/{{PROJECTNAME}}/$appname/g" $(grep -rl '{{PROJECTNAME}}' --exclude ${0##*/})
 }
 
-mv app.tmpl "$appname"
 virtualenv -p python3 .
 . bin/activate
 pip install -r requirements.txt
+
 # Fix for Python 3
 sed -i -e 's/\.iteritems/.items/g' lib/*/site-packages/flaskext/genshi.py
-# Replace placeholders for the real project name
-sed -i -e "s/{{PROJECTNAME}}/$appname/g" $(grep -rl '{{PROJECTNAME}}' --exclude $0)
 
 cat <<E
 
+
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 Initialization done. Here are your next steps:
+- Activate the virtualenv: . bin/activate
 - Define your views in $appname/views.py (@app.route defs)
 - Define your models in $appname/models.py (sqlalchemy models)
 - Initialize the database: python -c 'from $appname import db; db.create_all()'
+- Run the application:
+    env FLASK_APP=$appname/__init__.py FLASK_DEBUG=debug flask run -h 0.0.0.0
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 E
