0d0505bfae5e2a5e6520a8103bf4f5943d2a67bb
Alexandre Hamelin <alexandre.hamelin@gmail.com>
2019-01-17 17:52:41 +0000

    Just specify the directory name (app name), not the .py

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 . bin/activate
-export FLASK_APP=${FLASK_APP:-$(echo */__init__.py)} FLASK_ENV=${FLASK_ENV:-development}
+export FLASK_APP=${FLASK_APP:-$(dirname $(echo */__init__.py))} FLASK_ENV=${FLASK_ENV:-development}
 exec flask run -h :: "$@"
